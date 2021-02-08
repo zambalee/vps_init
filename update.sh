f3a2fd8c5a6e1899a485a3d3d408efeff524f432
zambalee <zamba@thinktop.biz>
2021-02-08 22:06:35 +0800

    fix spell error

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 !#/bin/sh
 
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt clean-y && sudo apt autoremove -y
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt clean -y && sudo apt autoremove -y
